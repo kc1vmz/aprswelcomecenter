c6692f12a824a4851e16a7360eb0ecb0ee089d23
John Rokicki <jcrokicki@gmail.com>
2026-09-17 14:20:18 -0400

    add upgrade capability

--- installers/linux/aprswc_installer.sh
+++ installers/linux/aprswc_installer.sh
@@ -17,15 +17,21 @@
 #
 # http://www.kc1vmz.com
 # 
+WC_UPGRADE=N
 WC_OS=$(uname)
 if [ "$WC_OS" = "Linux" ]; then
   echo "Welcome to the APRS Welcome Center installer for Linux"
   echo "Copyright (c) 2026 John Rokicki KC1VMZ - Licensed under GPL v3"
   echo "https://www.kc1vmz.com"
 
-  if [ -n "${APRSWC_INSTALL_DIR+x}" ]; then
-    echo "A previous version of APRS Welcome Center was detected. Please back up your database, uninstall then reinstall."
-    exit 1
+  if grep -q APRSWC_INSTALL_DIR /etc/environment; then
+    echo "A previous version of APRS Welcome Center was detected."
+    WC_UPGRADE=Y
+    read -e -i $WC_UPGRADE -p "Do you wish to upgrade APRS Welcome Center?: " WC_UPGRADE
+    if [[ "$WC_UPGRADE" =~ ^[Nn]$ ]]; then
+      echo "APRS Welcome Center installation will now exit."
+      exit 1
+    fi
   fi
 
   # default environment variables
@@ -36,19 +42,20 @@ if [ "$WC_OS" = "Linux" ]; then
   WC_PORT=8080
   WC_INSTALL_SERVICES=Y
 
-  read -e -i $WC_VERSION -p "What version of APRS Welcome Center?: " WC_VERSION
-  read -e -i $WC_INSTALL_DIR -p "Where should APRS Welcome Center be installed?: " WC_INSTALL_DIR
-  read -e -i $WC_DB_DIR -p "Where should APRS Welcome Center database space be located?: " WC_DB_DIR
-  read -e -i $WC_TEMP_DIR -p "Where should APRS Welcome Center temp space be located?: " WC_TEMP_DIR
-  read -e -i $WC_PORT -p "What port should the HTTP service listen on?: " WC_PORT
-  read -e -i $WC_INSTALL_SERVICES -p "Do you want APRS Welcome Center to be configured as services and started at boot time (Y/n)?: " WC_INSTALL_SERVICES
+  if [[ "$WC_INSTALL_SERVICES" =~ ^[Yy]$ ]]; then
+    read -e -i $WC_VERSION -p "What version of APRS Welcome Center?: " WC_VERSION
+    if [[ "$WC_UPGRADE" =~ ^[Nn]$ ]]; then
+      read -e -i $WC_INSTALL_DIR -p "Where should APRS Welcome Center be installed?: " WC_INSTALL_DIR
+      read -e -i $WC_DB_DIR -p "Where should APRS Welcome Center database space be located?: " WC_DB_DIR
+      read -e -i $WC_TEMP_DIR -p "Where should APRS Welcome Center temp space be located?: " WC_TEMP_DIR
+      read -e -i $WC_PORT -p "What port should the HTTP service listen on?: " WC_PORT
+      read -e -i $WC_INSTALL_SERVICES -p "Do you want APRS Welcome Center to be configured as services and started at boot time (Y/n)?: " WC_INSTALL_SERVICES
+    else
+      WC_INSTALL_DIR=$APRSWC_INSTALL_DIR
+    fi
+  fi
 
   sudo cp /etc/environment /etc/environment.pre_aprswc_install
-
-  APRSWC_INSTALL_DIR=$WC_INSTALL_DIR
-  echo "APRSWC_INSTALL_DIR=$APRSWC_INSTALL_DIR" | sudo tee -a /etc/environment >  /dev/null
-
-  mkdir $WC_INSTALL_DIR
 
   JAVA_VER=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F '.' '{sub("^$", "0", $2); print $1$2}')
   if [ "$JAVA_VER" -ge 21 ]; then
@@ -56,52 +63,69 @@ if [ "$WC_OS" = "Linux" ]; then
     echo "Java 21+ verified"
   else
     echo "Java 21 or greater not installed - exiting."
-    sudo cp /etc/environment.pre_aprswc_install /etc/environment 
     sudo rm /etc/environment.pre_aprswc_install
     exit 1
   fi
 
-  sudo mkdir $WC_TEMP_DIR
-  sudo chown $USER $WC_TEMP_DIR
-
-  sudo mkdir $WC_DB_DIR
-  sudo chown $USER $WC_DB_DIR
+  APRSWC_INSTALL_DIR=$WC_INSTALL_DIR
+  if [[ "$WC_UPGRADE" =~ ^[Nn]$ ]]; then
+    echo "APRSWC_INSTALL_DIR=$APRSWC_INSTALL_DIR" | sudo tee -a /etc/environment >  /dev/null
+    mkdir $WC_INSTALL_DIR
+    sudo mkdir $WC_TEMP_DIR
+    sudo chown $USER $WC_TEMP_DIR
+    sudo mkdir $WC_DB_DIR
+    sudo chown $USER $WC_DB_DIR
+  fi
 
   echo "Retrieving APRS Welcome Center binary"
   cd $WC_INSTALL_DIR
-
   WC_SRC_URL_ROOT=https://github.com/kc1vmz/aprswelcomecenter/releases/download/v$WC_VERSION
   wget -q -O aprs-welcome-center-$WC_VERSION.jar $WC_SRC_URL_ROOT/aprs-welcome-center-$WC_VERSION.jar
   WC_SRC_URL_ROOT=
 
   chmod +x aprs-welcome-center-$WC_VERSION.jar
 
-  APRSWC_DB_DIR=$WC_DB_DIR
-  APRSWC_TEMP_DIR=$WC_TEMP_DIR
-  APRSWC_PORT=$WC_PORT
+  if [[ "$WC_UPGRADE" =~ ^[Nn]$ ]]; then
+    APRSWC_DB_DIR=$WC_DB_DIR
+    APRSWC_TEMP_DIR=$WC_TEMP_DIR
+    APRSWC_PORT=$WC_PORT
+  else
+    if [ -n "/etc/systemd/system/aprs-welcome-center.service+x" ]; then
+      WC_INSTALL_SERVICES=Y
+    else
+      WC_INSTALL_SERVICES=N
+    fi
+  fi
 
   if [[ "$WC_INSTALL_SERVICES" =~ ^[Yy]$ ]]; then
-    echo '[Unit]' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Description=Net Central Server' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo '[Service]' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'User='$LOGNAME | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'WorkingDirectory='$WC_INSTALL_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Type=simple' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Restart=always' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'SuccessExitStatus=143' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'RemainAfterExit=yes' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'ExecStart=java -jar '$WC_INSTALL_DIR'/aprs-welcome-center-'$WC_VERSION'.jar' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Environment=APRSWC_DB_DIR='$APRSWC_DB_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Environment=APRSWC_TEMP_DIR='$APRSWC_TEMP_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Environment=SERVER_PORT='$APRSWC_PORT | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'Environment=APRSWC_INSTALL_DIR='$APRSWC_INSTALL_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo '[Install]' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-    echo 'WantedBy=multi-user.target' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
-
-    sudo systemctl daemon-reload
-    sudo systemctl enable aprs-welcome-center
-    sudo systemctl start aprs-welcome-center
-    echo "APRS Welcome Center running as background service."
+    if [[ "$WC_UPGRADE" =~ ^[Nn]$ ]]; then
+      echo '[Unit]' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Description=Net Central Server' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo '[Service]' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'User='$LOGNAME | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'WorkingDirectory='$WC_INSTALL_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Type=simple' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Restart=always' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'SuccessExitStatus=143' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'RemainAfterExit=yes' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'ExecStart=java -jar '$WC_INSTALL_DIR'/aprs-welcome-center-'$WC_VERSION'.jar' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Environment=APRSWC_DB_DIR='$APRSWC_DB_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Environment=APRSWC_TEMP_DIR='$APRSWC_TEMP_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Environment=SERVER_PORT='$APRSWC_PORT | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'Environment=APRSWC_INSTALL_DIR='$APRSWC_INSTALL_DIR | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo '[Install]' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      echo 'WantedBy=multi-user.target' | sudo tee -a /etc/systemd/system/aprs-welcome-center.service >  /dev/null
+      sudo systemctl daemon-reload
+      sudo systemctl enable aprs-welcome-center
+      sudo systemctl start aprs-welcome-center
+      echo "APRS Welcome Center running as background service."
+    else
+      sudo systemctl stop aprs-welcome-center
+      sudo sed -i 's/aprs-welcome-center-.*.jar/aprs-welcome-center-'$WC_VERSION'.jar/g' /etc/systemd/system/aprs-welcome-center.service
+      sudo systemctl daemon-reload
+      sudo systemctl start aprs-welcome-center
+      echo "APRS Welcome Center restarted."
+    fi
   else
     echo "Environment variables have been added to /etc/environment.  Make sure to reboot or run 'source /etc/environment' to add them into any new terminal."
     echo "You can run APRS Welcome Center using the  'java -jar $WC_INSTALL_DIR/aprs-welcome-center-$WC_VERSION.jar' command"
@@ -114,7 +138,7 @@ if [ "$WC_OS" = "Linux" ]; then
   WC_INSTALL_SERVICES=
   WC_DB_DIR=
   WC_PORT=
-
+  WC_UPGRADE=
 elif [ "$WC_OS" = "Darwin" ]; then
   echo "This is a Mac Machine - not yet supported"
   exit 1
