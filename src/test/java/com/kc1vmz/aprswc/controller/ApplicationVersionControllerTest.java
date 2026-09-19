87556f175f1b519b22d915dc7048649ab67afbbe
John Rokicki <jcrokicki@gmail.com>
2026-09-18 20:23:26 -0400

    version 1.0.3

--- src/test/java/com/kc1vmz/aprswc/controller/ApplicationVersionControllerTest.java
+++ src/test/java/com/kc1vmz/aprswc/controller/ApplicationVersionControllerTest.java
@@ -40,7 +40,7 @@ class ApplicationVersionControllerTest {
                 .jsonPath("$.applicationName")
                 .isEqualTo("APRSWelcomeCenter")
                 .jsonPath("$.version")
-                .isEqualTo("1.0.2")
+                .isEqualTo("1.0.3")
                 .jsonPath("$.author")
                 .isEqualTo("John Rokicki KC1VMZ")
                 .jsonPath("$.copyrightYear")
